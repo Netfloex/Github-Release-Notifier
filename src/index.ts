fc5be4bb64bcbdc68cc80e1aca2274c0960ca1f5
Netfloex <38650595+Netfloex@users.noreply.github.com>
2022-07-22 19:55:01 +0200

    Await sending notifications, removed console.log

--- src/index.ts
+++ src/index.ts
@@ -17,50 +17,54 @@ const main = async (): Promise<void> => {
 	)
 
 	const repos = await getReleases(ids)
-	repos.forEach(async (repo) => {
-		store.data.repositories ??= []
-		const latestRelease = repo.releases.nodes[0]
+	await Promise.all(
+		repos.map(async (repo) => {
+			store.data.repositories ??= []
+			const latestRelease = repo.releases.nodes[0]
 
-		if (!latestRelease) {
+			if (!latestRelease) {
+				console.log(
+					chalk`The repository {bold ${repo.nameWithOwner}} does not have any releases.`,
+				)
+
+				return
+			}
+			const cachedRepo = store.data.repositories.find(
+				(r) => r.id == repo.id,
+			)!
+			cachedRepo.releasesNotified ??= []
+
+			if (cachedRepo.releasesNotified.includes(latestRelease.id)) {
+				return console.log(
+					chalk`No new releases for {bold ${repo.nameWithOwner}}`,
+				)
+			}
+
+			cachedRepo.releasesNotified.push(latestRelease.id)
 			console.log(
-				chalk`The repository {bold ${repo.nameWithOwner}} does not have any releases.`,
+				chalk`New Release on {bold ${repo.nameWithOwner}} {green ${latestRelease.name}}`,
 			)
 
-			return
-		}
-		const cachedRepo = store.data.repositories.find((r) => r.id == repo.id)!
-		cachedRepo.releasesNotified ??= []
-
-		if (cachedRepo.releasesNotified.includes(latestRelease.id)) {
-			return console.log(
-				chalk`No new releases for {bold ${repo.nameWithOwner}}`,
-			)
-		}
-
-		cachedRepo.releasesNotified.push(latestRelease.id)
-		console.log(
-			chalk`New Release on {bold ${repo.nameWithOwner}} {green ${latestRelease.name}}`,
-		)
-
-		await axios.post(discordWebhooks[0], {
-			embeds: [
-				{
-					title: latestRelease.name,
-					description: latestRelease.description,
-					url: latestRelease.url,
-					color: 3309752,
-					author: {
-						name: repo.nameWithOwner,
-						icon_url: repo.owner.avatarUrl,
-						url: repo.url,
+			await axios.post(discordWebhooks[0], {
+				embeds: [
+					{
+						title: latestRelease.name,
+						description: latestRelease.description,
+						url: latestRelease.url,
+						color: 3309752,
+						author: {
+							name: repo.nameWithOwner,
+							icon_url: repo.owner.avatarUrl,
+							url: repo.url,
+						},
 					},
-				},
-			],
-			username: latestRelease.author.name ?? latestRelease.author.login,
-			avatar_url: latestRelease.author.avatarUrl,
-		})
-		console.log(latestRelease)
-	})
+				],
+				username:
+					latestRelease.author.name ?? latestRelease.author.login,
+				avatar_url: latestRelease.author.avatarUrl,
+			})
+		}),
+	)
 	await store.write()
 }
 
